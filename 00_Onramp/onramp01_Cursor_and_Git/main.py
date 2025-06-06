dbab3bfa19b88a87834faa25808fe4a25300106e
DJ <djdinnebeil@gmail.com>
2025-06-06 17:38:08 -0400

    Updating name and location in onramp01

--- 00_Onramp/onramp01_Cursor_and_Git/main.py
+++ 00_Onramp/onramp01_Cursor_and_Git/main.py
@@ -4,12 +4,12 @@ print("Hello, World!")
 #### ❓Question #1:
 # What is your name?
 print("What is your name?")
-name = # Add your name here
+name = "DJ"
 print("Hello, " + name + "!")
 
 #### ❓Question #2:
 # Where do you live?
 print("Where do you live?")
-location = # Add your location here
+location ="New Jersey"
 print("I live in " + location + ".")
 
